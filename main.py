74f4ea7894ac60d84a5a8d8a0ad87bb0fbe64743
Ramanathan Varadharajan <ramanathan.varadharajan@outlook.com>
2021-03-10 01:40:36 +0100

    Move static strings to central location.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import copy
 import sys
 
+from src.attributes import excel_input_names as ein
 from src.stock_calculator.inputs import Inputs
 from src.stock_calculator.outputs import Output
 from src.stock_calculator.yahoo_finance import YahooFinance
@@ -9,14 +10,12 @@ from src.stock_calculator.yahoo_finance import YahooFinance
 
 if __name__ == "__main__":
 
-    input_df = Inputs().read_inputs(
-        r"./src/dataframes/input/input_portfolio.xlsx"
-    )
+    input_df = Inputs().read_inputs(ein.filepath)
     print(r"Input.")
     print(input_df)
 
     output_df = copy.copy(input_df)
-    stocks = list(input_df["Stock"])
+    stocks = list(input_df[ein.df_stock])
     daily, monthly, quarterly = YahooFinance().all_history(stocks)
     output_df = Output().put_outputs(output_df, daily, monthly, quarterly)
 
