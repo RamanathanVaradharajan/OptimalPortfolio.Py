017c662b3afdbb2833fbbcdc1c43f9a38f7eea67
Ramanathan Varadharajan <ramanathan.varadharajan@outlook.com>
2021-03-12 02:34:21 +0100

    Package metadata.

--- src/__version__.py
+++ src/__version__.py
@@ -1,7 +1,8 @@
 """Package metadata."""
 
-__title__ = "TangencyPortfolio"
-__package__ = "src"
+# TODO: Update while publishing.
+__title__ = "OptimizeMyPortfolio"
+__package__ = "optimizemyportfolio"
 __version__ = "1.0"
 __author__ = "Ramanathan Varadharajan"
 __description__ = "Optimize the weights of the portfolio using historical return, volatility and correlation."
